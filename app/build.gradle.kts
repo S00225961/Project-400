e1192ef20695161f3b7d893b8ce53255c0df483a
S00225961 <s00225961@atu.ie>
2024-12-07 16:34:51 +0000

    Added tensorflow single pose model file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.project400"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.project400"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
